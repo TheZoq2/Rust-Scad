404646c3a58e36ad4ec7d45cb679d45e317ff948
TheZoq2 <frans.skarman@gmail.com>
2018-02-03 15:37:57 +0100

    Fix documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 /*!
     This crate is used to generate openscad models using rust.
 
-    ##Usage
+    ## Usage
 
     First, let's look at a simple example of the crate being used.
 
@@ -13,32 +13,31 @@
     //Avoid having to write scad_generator:: everywhere
     use scad_generator::*;
 
-    pub fn main()
-    {
+    pub fn main() {
         //Create an scad file object for storing the scad objects. This
         //allows us to set things like the detail level ($fn) for the models.
-        let mut sfile = ScadFile::new();
+        let mut scadfile = ScadFile::new();
 
         //Sets the $fn variable in scad which controls the detail level of things
         //like spheres. Look at the scad wiki for details
-        sfile.set_detail(50);
+        scad_file.set_detail(50);
 
         //Create an scad object
-        let mut cube = scad!(Translate(vec3(2.0, 2.0, 3.0));
-                {
-                    scad!(Cube(vec3(2.0,1.0,4.0)))
-                });
+        let mut cube = scad!(Translate(vec3(2.0, 2.0, 3.0)); {
+            scad!(Cube(vec3(2.0,1.0,4.0)))
+        });
 
         //Create a cylinder with a height of 10 and a diameter of 3 mm
         let cylinder = scad!(Cylinder(10., Diameter(3.)));
 
         //Add the cylinder to the cubes translation.
         cube.add_child(cylinder);
-        
+
         //Add the cube object to the file
-        sfile.add_object(cube.clone());
+        scad_file.add_object(cube.clone());
+
         //Save the scad code to a file
-        sfile.write_to_file(String::from("out.scad"));
+        scad_file.write_to_file(String::from("out.scad"));
 
         //You can also print the code for the object manually since it's just a string
         println!("{}", scad!(Cube(vec3(5., 3.,  2.))).get_code());
@@ -54,17 +53,18 @@
     }
     ```
 
-    ##The scad! macro
+    ## The `scad!` macro
     The most important part of the crate is the `scad!` macro. The first parameter
     of the macro is the element type of the object we want to create which should be 
     an instance of the `ScadElement` enum. If you only want to create a single scad
     object, you can simply end the macro invocation after the parent like this:
 
+
     ```
     # #[macro_use]
     # extern crate scad_generator;
     # use scad_generator::*;
-    
+
     # fn main(){
         scad!(Cube(vec3(10., 10., 10.)));
     # }
@@ -80,8 +80,7 @@
     # extern crate scad_generator;
     # use scad_generator::*;
 
-    # fn main()
-    # {
+    # fn main() {
         let child = scad!(Cylinder(10., Radius(3.)));
 
         scad!(Difference;{
@@ -101,24 +100,24 @@
     # }
     ```
 
-    ##Object parameters
+    ## Object parameters
     Almost all `ScadElements` take additional parameters that describe them. They 
     are enum parameters so you specify them as you would with enums. Some parameters
     are regular built in types like `f32` but there are some special ones which are 
     described below.
 
-    ###Vectors
+    ### Vectors
     The most common parameter is a vector. This library uses the nalgebra crate for vectors
     but writing `na::Vector3::new(x, y, z)` each time you want a vector is tedious which is 
     why the library contains the functions `vec3(x, y, z)` and `vec2(x, y)`. They are simply
     functions that call the equivalent nalgebra constructor.
 
-    ###Circle radii and diameters.
+    ### Circle radii and diameters.
     Just like regular OpenSCAD, you can create round objects by either specifying the diameter
     or radius of the circle. This is done using the `CircleType` enum which is either 
-    `Diameter(d)` or `Radius(r)`. 
+    `Diameter(d)` or `Radius(r)`.
 
-    ##Creating objects in loops
+    ## Creating objects in loops
     In most cases, the `scad!` macro should be good enoough to create objects, but one
     case where it is not,  is when you want to create several objects in a loop and 
     add them as children to a specific object. In this case, you have to use the 
@@ -129,17 +128,14 @@
     # extern crate scad_generator;
     # use scad_generator::*;
 
-    # fn main()
-    # {
+    # fn main() {
         //Create the parent and make sure its mutable
         let mut parent = scad!(Union);
 
-        for i in 0..3
-        {
+        for i in 0..3 {
             parent.add_child(scad!(Cube(vec3(0., i as f32, 0.))));
         }
     # }
-    
     ```
 */
 
